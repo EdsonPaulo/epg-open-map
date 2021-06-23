e3e9c1761ab8ab9c4bdc42b6a4ab9038263a812a
edsonpaulo <edsonpaulo24@gmail.com>
2021-06-23 14:01:09 +0100

    feat: :tada: project init

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import './styles/index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './pages';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
